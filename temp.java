c4345a3ced6d028974d8ae272e306fff1b932be1
idan vaknin <idanvaknin18@walla.com>
2024-02-01 19:43:30 +0200

    first commit

---
+++ temp.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+		 // Print the multiplication table
+		int n = 10;
+        for (int i = 0; i <= n; i++) {
+            System.out.printf("\n\t%d", i);
+            for (int j = 0; j <= n; j++) {
+                int result = i * j;
+                System.out.printf("\t%d", result);
+            }
+            System.out.println();
+        }
+	}
+}
